ab5b691fd64601813c90a5f9dbc19b96c3269f15
Alex Gilvarry <gilvarry@wisc.edu>
2019-04-01 17:31:38 -0600

    cleanup
    cleanup

--- php/callProxy.php
+++
@@ -1,9 +0,0 @@
-<?php
-print "I'm about to learn PHP!";
-include '/Project2/php/cartoDBProxy.php';
-//			^CHANGE THIS TO THE PATH TO YOUR cartodbProxy.php
-$queryURL = $_POST['qurl'];
-$return = goProxy($queryURL);
-
-echo $return;
-?>
